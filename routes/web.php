f8582339ff9d2ec2c53725aace06c2b268a8604b
Elvrt <elvarettas23@gmail.com>
2024-02-28 15:05:29 +0700

    Percobaan Routing

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,43 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+
+Route::get('/world', function () {
+    return 'World';
+   });
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';});
+
+
+Route::get('/about', function () {
+    return 'NIM : 2241720116<br> Nama: Elvaretta Salsabilla';});
+
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama Saya '.$name;});
+
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId.'Komentar ke-'.$commentId.'';
+});
+
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel '.$id;
+} );
+
+
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama Saya'.$name;
+});
+
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama Saya '.$name;
 });
