d5618fadb4eac48e6b0b22047619b83d35a76aed
Elvrt <elvarettas23@gmail.com>
2024-02-28 15:56:26 +0700

    Percobaan Controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,16 +18,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/hello', function () {
-    return 'Hello World';
-});
-
+// Route::get('/hello', function () {
+//     return 'Hello World';
+// });
 
 Route::get('/world', function () {
     return 'World';
    });
 
-
+   
 Route::get('/', function () {
     return 'Selamat Datang';});
 
@@ -53,3 +57,26 @@ Route::get('/user/{name?}', function ($name=null) {
 Route::get('/user/{name?}', function ($name='John') {
     return 'Nama Saya '.$name;
 });
+
+Route::get('/hello', [WelcomeController::class,'hello']); 
+
+Route::get('/index', [PageController::class,'index']);
+Route::get('/about', [PageController::class,'about']);
+Route::get('/articles/{id}', [PageController::class,'articles']);
+
+Route::get('/index', [HomeController::class,'']);
+
+Route::get('/about', [AboutController::class,'']);
+
+Route::get('/articles/{id}', [ArticleController::class,'articles']);
+
+
+use App\Http\Controllers\PhotoController;
+
+// Route::resource('photos', PhotoController::class);
+
+
+Route::resource('photos', PhotoController::class) -> only(['index', 'show']);
+
+
+Route::resource('photos', PhotoController::class)->except(['create', 'store', 'update', 'destroy' ]);
